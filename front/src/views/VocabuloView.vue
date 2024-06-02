fbe5afa106440ad4ab6a336e133a64ce9ab6d9c6
Diego Silva <diegosilva157890@gmail.com>
2024-06-01 21:48:40 -0300

    Buscar por termor e versão

--- front/src/views/VocabuloView.vue
+++ front/src/views/VocabuloView.vue
@@ -1,14 +1,28 @@
 <template>
   <div class="about">
     <h1>Vocábulos</h1>
-    <p><label for="termo">Termo: </label><input id="termo" type="text" v-model="vocabulo.termo" /></p>
-    <p><label for="significado">Significado: </label><input id="significado" type="text" v-model="vocabulo.significado" /></p>
-    <p><label for="versao">Versão: </label><input id="versao" type="number" v-model="vocabulo.versao" /></p>
-    <p><label for="dataHoraDesativacao">Data/Hora Desativação: </label><input id="dataHoraDesativacao" type="datetime-local" v-model="vocabulo.dataHoraDesativacao" /></p>
-    <button>Atualizar</button>
-    <button @click="incluir">Incluir</button>
+    <p>
+      <label for="termo">Termo: </label>
+      <input id="termo" type="text" v-model="vocabulo.termo" />
+    </p>
+    <p>
+      <label for="significado">Significado: </label>
+      <input id="significado" type="text" v-model="vocabulo.significado" />
+    </p>
+    <p>
+      <label for="versao">Versão: </label>
+      <input id="versao" type="number" v-model="vocabulo.versao" />
+    </p>
+    <p>
+      <label for="dataHoraDesativacao">Data/Hora Desativação: </label>
+      <input id="dataHoraDesativacao" type="datetime-local" v-model="vocabulo.dataHoraDesativacao" />
+    </p>
 
-    <table>
+    <button @click="incluir">Incluir</button>
+    <button @click="buscarPorTermoEVersao">Buscar</button>
+
+    <div v-if="mensagem">{{ mensagem }}</div>
+    <table v-else>
       <thead>
         <tr>
           <th>Id</th>
@@ -37,6 +51,7 @@ import { onMounted, ref } from 'vue';
 import axios from 'axios';
 
 const vocabulos = ref();
+const mensagem = ref('');
 
 const vocabulo = ref({
   termo: '',
@@ -49,12 +64,33 @@ const vocabulo = ref({
 async function buscarVocabulos() {
   try {
     const response = await axios.get("/vocabulo");
-    vocabulos.value = response.data.map((situacaoVocabulo:any) => ({
+    vocabulos.value = response.data.map((situacaoVocabulo: any) => ({
       ...situacaoVocabulo,
       situacao: situacaoVocabulo.dataHoraDesativacao ? 'desativado' : 'ativo',
     }));
+    mensagem.value = '';
   } catch (error) {
     console.error('Erro ao buscar vocábulos:', error);
+    mensagem.value = 'Erro ao buscar vocábulos';
+  }
+}
+
+async function buscarPorTermoEVersao() {
+  try {
+    const response = await axios.get(`/vocabulo/${vocabulo.value.termo}/${vocabulo.value.versao}`);
+    if (response.data.length > 0) {
+      vocabulos.value = response.data.map((situacaoVocabulo: any) => ({
+        ...situacaoVocabulo,
+        situacao: situacaoVocabulo.dataHoraDesativacao ? 'desativado' : 'ativo',
+      }));
+      mensagem.value = '';
+    } else {
+      vocabulos.value = [];
+      mensagem.value = 'Nenhum vocábulo foi encontrado para os critérios fornecidos';
+    }
+  } catch (error) {
+    console.error('Erro ao buscar vocábulo por termo e versão:', error);
+    mensagem.value = 'Erro ao buscar vocábulo por termo e versão';
   }
 }
 
@@ -62,7 +98,7 @@ async function incluir() {
   try {
     const response = await axios.post("/vocabulo", vocabulo.value);
     console.log('Vocábulo criado com sucesso:', response.data);
-    buscarVocabulos(); 
+    buscarVocabulos();
     vocabulo.value = {
       termo: '',
       significado: '',
